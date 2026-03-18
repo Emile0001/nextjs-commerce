3c2122d526157f23fa1082ae44af04639a452eb2
Emile0001 <boteseh@gmail.com>
2026-03-18 14:49:06 +0200

    product card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-    return <div>Colourful Nails</div>;
+    return (
+        <div>
+            <ProductList
+                data={sampleData.products}
+                title="Featured Products"
+                limit={4}
+            />
+        </div>
+    );
 };
 
 export default Homepage;
